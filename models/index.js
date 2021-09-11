4b0635693edec88d4daf4a3c1148b237f9a7bd6f
Jackie Hodges <jackie.hodges2015@gmail.com>
2021-09-11 15:03:53 -0500

    removed weekly meal function/data

--- models/index.js
+++ models/index.js
@@ -1,27 +1,10 @@
 const User = require("./User");
 const Recipes = require("./Recipe");
-const WeeklyMeals = require('./WeeklyMeals');
 const Tags = require('./Tags')
 
-User.belongsToMany(Recipes, {
-    through: {
-        model: WeeklyMeals,
-        unique: false
-    }
-});
-
-Recipes.belongsToMany(User, {
-    through: {
-        model: WeeklyMeals,
-        unique: false
-    }
-});
-
-// User.hasMany(Recipes);
-// Recipes.belongsToMany(User);
-User.hasOne(WeeklyMeals);
-WeeklyMeals.belongsTo(User);
+User.hasMany(Recipes);
+Recipes.belongsTo(User);
 Recipes.hasMany(Tags);
 Tags.belongsTo(Recipes);
 
-module.exports = { User, Recipes, Tags, WeeklyMeals };
+module.exports = { User, Recipes, Tags };
--- scripts/seed.js
+++ scripts/seed.js
@@ -1,9 +1,8 @@
 const sequelize = require('../config/connection');
-const { User, Recipes, WeeklyMeals, Tags } = require('../models');
+const { User, Recipes, Tags } = require('../models');
 
 const userData = require('./userData.json');
 const recipeData = require('./recipeData.json');
-const weeklyData = require('./weeklyMealsData.json');
 const tagData = require('./tagData.json');
 
 const seedDatabase = async () => {
@@ -12,8 +11,6 @@ const seedDatabase = async () => {
   await User.bulkCreate(userData);
 
   await Recipes.bulkCreate(recipeData);
-
-  await WeeklyMeals.bulkCreate(weeklyData);
 
   await Tags.bulkCreate(tagData);
   
