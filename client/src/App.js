d87d6ffce75913abeedecbc019f1d7459fbe8804
Jackie Hodges <jackie.hodges2015@gmail.com>
2021-09-22 12:46:35 -0500

    used protected routes to force user to login

--- client/src/App.js
+++ client/src/App.js
@@ -7,18 +7,28 @@ import MyRecipes from "./pages/MyRecipes";
 import SearchRecipe from "./components/searchRecipes";
 import ThisWeek from "./components/thisWeeksMeals";
 import SpecificRecipe from "./pages/SpecificRecipe";
+import ProtectedRoute from "./auth/protected-route";
+import { useAuth0 } from "@auth0/auth0-react";
+import Loading from "./components/loading";
 
 function App() {
+
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
       <NavBar />
         <Switch>
           <Route exact path="/" component={ HomePage } />
-          <Route exact path="/myRecipes" component={ MyRecipes } />
-          <Route exact path="/myRecipes/:id" component={ SpecificRecipe } />
+          <ProtectedRoute exact path="/myRecipes" component={ MyRecipes } />
+          <ProtectedRoute exact path="/myRecipes/:id" component={ SpecificRecipe } />
           <Route exact path="/searchRecipes" component={ SearchRecipe } />
-          <Route exact path="/thisWeeksMeals" component={ ThisWeek } />
+          <ProtectedRoute exact path="/thisWeeksMeals" component={ ThisWeek } />
         </Switch>
       </BrowserRouter>
     </div>
--- client/src/pages/MyRecipes.js
+++ client/src/pages/MyRecipes.js
@@ -1,25 +1,30 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import SearchRecipe from "../components/searchRecipes";
 
 function MyRecipes() {
 
+    const { user, isAuthenticated } = useAuth0();
+
     return (
-        <div>
-            <Container>
-                <Row>
-                    <Col>My Recipes</Col>
-                </Row>
-                <Row>
-                    {/* <Col>
+        // isAuthenticated && (
+            <div>
+                <Container>
+                    <Row>
+                        <Col>{user.given_name}'s Recipes</Col>
+                    </Row>
+                    <Row>
+                        {/* <Col>
                         <NewRecipe />
                     </Col> */}
-                    <Col>
-                        <SearchRecipe />
-                    </Col>
-                </Row>
-            </Container>
-        </div>
+                        <Col>
+                            <SearchRecipe />
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        // )
     )
 }
 
