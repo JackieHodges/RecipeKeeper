1f008ce84d91439928fd5713a39e3de5d9cd1fa9
Jackie Hodges <jackie.hodges2015@gmail.com>
2021-10-20 10:19:44 -0500

    created favorite model

--- models/index.js
+++ models/index.js
@@ -1,10 +1,14 @@
 const User = require("./User");
 const Recipes = require("./Recipe");
-const Tags = require('./Tags')
+const Tags = require('./Tags');
+const Favorite = require("./Favorite");
 
 User.hasMany(Recipes);
 Recipes.belongsTo(User);
 Recipes.hasMany(Tags);
 Tags.belongsTo(Recipes);
 
-module.exports = { User, Recipes, Tags };
+User.hasMany(Favorite);
+Favorite.belongsTo(User);
+
+module.exports = { User, Recipes, Tags, Favorite };
--- scripts/seed.js
+++ scripts/seed.js
@@ -1,9 +1,10 @@
 const sequelize = require('../config/connection');
-const { User, Recipes, Tags } = require('../models');
+const { User, Recipes, Tags, Favorite } = require('../models');
 
 const userData = require('./userData.json');
 const recipeData = require('./recipeData.json');
 const tagData = require('./tagData.json');
+const favoriteData = require('./favoriteData.json')
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -11,6 +12,8 @@ const seedDatabase = async () => {
   await User.bulkCreate(userData);
 
   await Recipes.bulkCreate(recipeData);
+
+  await Favorite.bulkCreate(favoriteData);
 
   await Tags.bulkCreate(tagData);
   
