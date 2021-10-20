de3f3399a1d87f0fa42a488dcdb54cdbbeabb102
Jackie Hodges <jackie.hodges2015@gmail.com>
2021-10-20 10:43:41 -0500

    removed favorites because it caused an issue when deleting

--- models/index.js
+++ models/index.js
@@ -1,14 +1,10 @@
 const User = require("./User");
 const Recipes = require("./Recipe");
 const Tags = require('./Tags');
-const Favorite = require("./Favorite");
 
 User.hasMany(Recipes);
 Recipes.belongsTo(User);
 Recipes.hasMany(Tags);
 Tags.belongsTo(Recipes);
 
-User.hasMany(Favorite);
-Favorite.belongsTo(User);
-
-module.exports = { User, Recipes, Tags, Favorite };
+module.exports = { User, Recipes, Tags };
--- scripts/seed.js
+++ scripts/seed.js
@@ -1,10 +1,9 @@
 const sequelize = require('../config/connection');
-const { User, Recipes, Tags, Favorite } = require('../models');
+const { User, Recipes, Tags } = require('../models');
 
 const userData = require('./userData.json');
 const recipeData = require('./recipeData.json');
 const tagData = require('./tagData.json');
-const favoriteData = require('./favoriteData.json')
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,8 +11,6 @@ const seedDatabase = async () => {
   await User.bulkCreate(userData);
 
   await Recipes.bulkCreate(recipeData);
-
-  await Favorite.bulkCreate(favoriteData);
 
   await Tags.bulkCreate(tagData);
   
